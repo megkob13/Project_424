7b4e66ad11982124cad29ba8c244bd8eb7cff79e
sms8749 <143756166+sms8749@users.noreply.github.com>
2023-11-14 13:22:57 -0500

    Update main.cpp
    added struct in main to collect user info

--- Project_424/main.cpp
+++ Project_424/main.cpp
@@ -1,4 +1,4 @@
-// Final Project 
+// Final Project
 // Presentation Date: 12/5/2023
 // AERSP 424 Fall 2023
 // Serin Santhosh and Megan O'Brien
@@ -11,49 +11,53 @@
 #include "spacecraft.h"
 #include "aircraft.h"
 
-
 using namespace std;
 
+struct UserInfo
+{
+    string fname;
+    string lname;
+    
+};
 
 int main()
 {
-	int choice; 
-	string name;
+    int choice;
+    //string name;
+    UserInfo user;
+    cout << "Please enter your first name: ";
+    cin >> user.fname;
+    
+    cout << "Please enter your last name: ";
+    cin >> user.lname;
 
-	cout << "Please enter your first name." << endl;
-	cin >> name;
+    cout << "Welcome to senior year in Aerospace Engineering at Penn State, " << user.fname << " " << user.lname << "!" << endl;
+    cout << "Which capstone would you like to be in?" << endl;
+    do
+    {
+        cout << "1. Aircraft" << endl;
+        cout << "2. Spacecraft" << endl;
 
-	
-	cout << "Welcome to senior year in Aerospace Engineering at Penn State, " << name << "!" << endl;
-	cout << "Which capstone would you like to be in?" << endl;
-	do
-	{
-		cout << "1. Aircraft" << endl;
-		cout << "2. Spacecraft" << endl;
+        cout << "Enter your choice: " << endl;
+        cin >> choice;
 
-		cout << "Enter your choice: " << endl;
-		cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << endl;
+            cout << "You selected Aircraft" << endl;
+            AircraftRoute();
+            break;
+        case 2:
+            cout << endl;
+            cout << "You selected Spacecraft" << endl;
+            SpacecraftRoute();
+            break;
+        default:
+            cout << endl;
+            cout << "Invalid choice. Please enter a valid option." << endl;
+        }
+    } while (choice != 1 && choice != 2);
 
-		switch (choice)
-		{
-		case 1:
-			cout << endl;
-			cout << "You selected Aircraft" << endl;
-			AircraftRoute();
-			break;
-		case 2:
-			cout << endl;
-			cout << "You selected Spacecraft" << endl;
-			SpacecraftRoute();
-			break;
-		default:
-			cout << endl;
-			cout << "Invalid choice. Please enter a valid option." << endl;
-		}
-	} while (choice != 1 && choice != 2);
-
-	return 0;
+    return 0;
 }
-
-
-
