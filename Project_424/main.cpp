606b12dd79ba9e594b921b58bd5d61578e934c31
sms8749 <143756166+sms8749@users.noreply.github.com>
2023-11-14 13:42:36 -0500

    Add comments, and struct to hold user information

--- Project_424/main.cpp
+++ Project_424/main.cpp
@@ -5,6 +5,7 @@
 
 //Choose Your Own Adventure - Life of a Senior in Aerospace Engineering @ PSU!
 
+// include headerfiles and librarys to compile code
 #include <iostream>
 #include <string>
 #include <vector>
@@ -13,6 +14,7 @@
 
 using namespace std;
 
+//Struct used to hold user information
 struct UserInfo
 {
     string fname;
@@ -23,16 +25,19 @@ struct UserInfo
 int main()
 {
     int choice;
-    //string name;
     UserInfo user;
+    //collect information
     cout << "Please enter your first name: ";
     cin >> user.fname;
     
     cout << "Please enter your last name: ";
     cin >> user.lname;
 
+    //start the game!
     cout << "Welcome to senior year in Aerospace Engineering at Penn State, " << user.fname << " " << user.lname << "!" << endl;
     cout << "Which capstone would you like to be in?" << endl;
+    
+    // use do while loop to make sure loop starts, and will check at the end if a valid choice was made, if not, will redo the loop again!
     do
     {
         cout << "1. Aircraft" << endl;
@@ -46,16 +51,19 @@ int main()
         case 1:
             cout << endl;
             cout << "You selected Aircraft" << endl;
+                //will go to the aircraft cpp file
             AircraftRoute();
             break;
         case 2:
             cout << endl;
             cout << "You selected Spacecraft" << endl;
+                //will go to the spacecraft cpp file
             SpacecraftRoute();
             break;
         default:
             cout << endl;
             cout << "Invalid choice. Please enter a valid option." << endl;
+                //start do while loop again!
         }
     } while (choice != 1 && choice != 2);
 
